c711bf359a0046e550b84a5983f9c166b1e0793e
Nekrasov Vladimir <nekrasowladimir@gmail.com>
2022-04-02 17:18:20 +1030

    Linted test, model and dataset

--- dataset.py
+++ dataset.py
@@ -1,42 +1,41 @@
 import bisect
-import decord
-import torch
-import torch.nn as nn
-import torch.utils.data as Data
-import torchvision
-import torchvision.transforms as transforms
-import numpy as np
-from torch.utils import data
-from PIL import Image
-import numpy as np
 import os
-from collections import OrderedDict
-import util.util as util
+
 import cv2
-
+import decord
+import numpy as np
+import torchvision.transforms as transforms
 from PIL import Image
-from base_dataset import BaseDataset, get_params, get_transform
-import json
+from torch.utils import data
 
-IMG_EXTENSIONS = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG']
-VIDEO_EXTENSIONS = ['.mp4',]
+from base_dataset import get_params, get_transform
+
+IMG_EXTENSIONS = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
+VIDEO_EXTENSIONS = [
+    ".mp4",
+]
+
 
 def unpickle(file):
     import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
+
+    with open(file, "rb") as fo:
+        dict = pickle.load(fo, encoding="bytes")
     return dict
+
 
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
+
 def is_video_file(filename):
     return any(filename.endswith(extension) for extension in VIDEO_EXTENSIONS)
+
 
 def make_dataset(dir, stop=10000, filter_fn=is_image_file):
     images = []
     count = 0
-    assert os.path.isdir(dir), '%s is not a valid directory' % dir
+    assert os.path.isdir(dir), "%s is not a valid directory" % dir
     for root, _, fnames in sorted(os.walk(dir)):
         for fname in fnames:
             if filter_fn(fname):
@@ -47,8 +46,18 @@ def make_dataset(dir, stop=10000, filter_fn=is_image_file):
                 return images
     return images
 
+
 class UnpairedDepthDataset(data.Dataset):
-    def __init__(self, root, root2, opt, transforms_r=None, mode='train', midas=False, depthroot=''):
+    def __init__(
+        self,
+        root,
+        root2,
+        opt,
+        transforms_r=None,
+        mode="train",
+        midas=False,
+        depthroot="",
+    ):
 
         self.root = root
         self.mode = mode
@@ -64,8 +73,9 @@ class UnpairedDepthDataset(data.Dataset):
             if os.path.exists(depthroot):
                 depth = make_dataset(depthroot)
             else:
-                print('could not find %s'%depthroot)
+                print("could not find %s" % depthroot)
                 import sys
+
                 sys.exit(0)
 
             newimages = []
@@ -74,12 +84,12 @@ class UnpairedDepthDataset(data.Dataset):
             for dmap in depth:
                 lastname = os.path.basename(dmap)
                 trainName1 = os.path.join(self.root, lastname)
-                trainName2 = os.path.join(self.root, lastname.split('.')[0] + '.jpg')
-                if (os.path.exists(trainName1)):
+                trainName2 = os.path.join(self.root, lastname.split(".")[0] + ".jpg")
+                if os.path.exists(trainName1):
                     newimages += [trainName1]
-                elif (os.path.exists(trainName2)):
+                elif os.path.exists(trainName2):
                     newimages += [trainName2]
-            print('found %d correspondences' % len(newimages))
+            print("found %d correspondences" % len(newimages))
 
             self.depth_maps = depth
             all_img = newimages
@@ -90,9 +100,9 @@ class UnpairedDepthDataset(data.Dataset):
         self.transform_r = transforms.Compose(transforms_r)
 
         self.opt = opt
-        
-        if mode == 'train':
-            
+
+        if mode == "train":
+
             self.img2 = make_dataset(root2)
 
             if len(self.data) > len(self.img2):
@@ -102,54 +112,64 @@ class UnpairedDepthDataset(data.Dataset):
                 howmanyrepeat = (len(self.img2) // len(self.data)) + 1
                 self.data = self.data * howmanyrepeat
                 self.labels = self.labels * howmanyrepeat
-            
 
             cutoff = min(len(self.data), len(self.img2))
 
-            self.data = self.data[:cutoff] 
-            self.img2 = self.img2[:cutoff] 
+            self.data = self.data[:cutoff]
+            self.img2 = self.img2[:cutoff]
 
-            self.min_length =cutoff
+            self.min_length = cutoff
         else:
             self.min_length = len(self.data)
-
 
     def __getitem__(self, index):
 
         img_path = self.data[index]
 
         basename = os.path.basename(img_path)
-        base = basename.split('.')[0]
+        base = basename.split(".")[0]
 
-        img_r = Image.open(img_path).convert('RGB')
+        img_r = Image.open(img_path).convert("RGB")
         transform_params = get_params(self.opt, img_r.size)
-        A_transform = get_transform(self.opt, transform_params, grayscale=(self.opt.input_nc == 1), norm=False)
-        B_transform = get_transform(self.opt, transform_params, grayscale=(self.opt.output_nc == 1), norm=False)        
+        A_transform = get_transform(
+            self.opt, transform_params, grayscale=(self.opt.input_nc == 1), norm=False
+        )
+        B_transform = get_transform(
+            self.opt, transform_params, grayscale=(self.opt.output_nc == 1), norm=False
+        )
 
-        if self.mode != 'train':
+        if self.mode != "train":
             A_transform = self.transform_r
 
-        img_r = A_transform(img_r )
+        img_r = A_transform(img_r)
 
-        B_mode = 'L'
+        B_mode = "L"
         if self.opt.output_nc == 3:
-            B_mode = 'RGB'
+            B_mode = "RGB"
 
         img_depth = 0
         if self.midas:
             img_depth = cv2.imread(self.depth_maps[index])
-            img_depth = A_transform(Image.fromarray(img_depth.astype(np.uint8)).convert('RGB'))
-
+            img_depth = A_transform(
+                Image.fromarray(img_depth.astype(np.uint8)).convert("RGB")
+            )
 
         img_normals = 0
         label = 0
 
-        input_dict = {'r': img_r, 'depth': img_depth, 'path': img_path, 'index': index, 'name' : base, 'label': label}
+        input_dict = {
+            "r": img_r,
+            "depth": img_depth,
+            "path": img_path,
+            "index": index,
+            "name": base,
+            "label": label,
+        }
 
-        if self.mode=='train':
+        if self.mode == "train":
             cur_path = self.img2[index]
             cur_img = B_transform(Image.open(cur_path).convert(B_mode))
-            input_dict['line'] = cur_img
+            input_dict["line"] = cur_img
 
         return input_dict
 
@@ -181,6 +201,6 @@ class VideoDataset(data.Dataset):
         else:
             frame_index = i
         frame = self.videos[video_index][frame_index]
-        img_r = Image.fromarray(frame.asnumpy()).convert('RGB')
+        img_r = Image.fromarray(frame.asnumpy()).convert("RGB")
         img_r = self.transform_r(img_r)
         return {"r": img_r, "depth": 0, "name": f"frame_{i:d}"}
--- test.py
+++ test.py
@@ -1,58 +1,134 @@
 #!/usr/bin/python3
 
 import argparse
-import sys
 import os
+import sys
 
-import torchvision.transforms as transforms
-from torchvision.utils import save_image
-from torch.utils.data import DataLoader
-from torch.autograd import Variable
 import torch
-
-from model import Generator, GlobalGenerator2, InceptionV3
-from dataset import UnpairedDepthDataset, VideoDataset
+import torchvision.transforms as transforms
 from PIL import Image
-import numpy as np
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+
+from dataset import UnpairedDepthDataset, VideoDataset
+from model import Generator, GlobalGenerator2, InceptionV3
 from utils import channel2width
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', required=True, type=str, help='name of this experiment')
-parser.add_argument('--checkpoints_dir', type=str, default='checkpoints', help='Where the model checkpoints are saved')
-parser.add_argument('--results_dir', type=str, default='results', help='where to save result images')
-parser.add_argument('--geom_name', type=str, default='feats2Geom', help='name of the geometry predictor')
-parser.add_argument('--batchSize', type=int, default=1, help='size of the batches')
-parser.add_argument('--dataroot', type=str, default='', help='root directory of the dataset')
-parser.add_argument('--depthroot', type=str, default='', help='dataset of corresponding ground truth depth maps')
+parser.add_argument("--name", required=True, type=str, help="name of this experiment")
+parser.add_argument(
+    "--checkpoints_dir",
+    type=str,
+    default="checkpoints",
+    help="Where the model checkpoints are saved",
+)
+parser.add_argument(
+    "--results_dir", type=str, default="results", help="where to save result images"
+)
+parser.add_argument(
+    "--geom_name", type=str, default="feats2Geom", help="name of the geometry predictor"
+)
+parser.add_argument("--batchSize", type=int, default=1, help="size of the batches")
+parser.add_argument(
+    "--dataroot", type=str, default="", help="root directory of the dataset"
+)
+parser.add_argument(
+    "--depthroot",
+    type=str,
+    default="",
+    help="dataset of corresponding ground truth depth maps",
+)
 
-parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
-parser.add_argument('--output_nc', type=int, default=1, help='number of channels of output data')
-parser.add_argument('--geom_nc', type=int, default=3, help='number of channels of geometry data')
-parser.add_argument('--every_feat', type=int, default=1, help='use transfer features for the geometry loss')
-parser.add_argument('--num_classes', type=int, default=55, help='number of classes for inception')
-parser.add_argument('--midas', type=int, default=0, help='use midas depth map')
+parser.add_argument(
+    "--input_nc", type=int, default=3, help="number of channels of input data"
+)
+parser.add_argument(
+    "--output_nc", type=int, default=1, help="number of channels of output data"
+)
+parser.add_argument(
+    "--geom_nc", type=int, default=3, help="number of channels of geometry data"
+)
+parser.add_argument(
+    "--every_feat",
+    type=int,
+    default=1,
+    help="use transfer features for the geometry loss",
+)
+parser.add_argument(
+    "--num_classes", type=int, default=55, help="number of classes for inception"
+)
+parser.add_argument("--midas", type=int, default=0, help="use midas depth map")
 
-parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
-parser.add_argument('--n_blocks', type=int, default=3, help='number of resnet blocks for generator')
-parser.add_argument('--size', type=int, default=256, help='size of the data (squared assumed)')
-parser.add_argument('--cuda', action='store_true', help='use GPU computation', default=True)
-parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load from')
-parser.add_argument('--aspect_ratio', type=float, default=1.0, help='The ratio width/height. The final height of the load image will be crop_size/aspect_ratio')
+parser.add_argument(
+    "--ngf", type=int, default=64, help="# of gen filters in first conv layer"
+)
+parser.add_argument(
+    "--n_blocks", type=int, default=3, help="number of resnet blocks for generator"
+)
+parser.add_argument(
+    "--size", type=int, default=256, help="size of the data (squared assumed)"
+)
+parser.add_argument(
+    "--cuda", action="store_true", help="use GPU computation", default=True
+)
+parser.add_argument(
+    "--n_cpu",
+    type=int,
+    default=8,
+    help="number of cpu threads to use during batch generation",
+)
+parser.add_argument(
+    "--which_epoch", type=str, default="latest", help="which epoch to load from"
+)
+parser.add_argument(
+    "--aspect_ratio",
+    type=float,
+    default=1.0,
+    help="The ratio width/height. The final height of the load image will be crop_size/aspect_ratio",
+)
 
-parser.add_argument('--mode', type=str, default='test', help='train, val, test, etc')
-parser.add_argument('--load_size', type=int, default=256, help='scale images to this size')
-parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
-parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
-parser.add_argument('--preprocess', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
-parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
-parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization')
+parser.add_argument("--mode", type=str, default="test", help="train, val, test, etc")
+parser.add_argument(
+    "--load_size", type=int, default=256, help="scale images to this size"
+)
+parser.add_argument("--crop_size", type=int, default=256, help="then crop to this size")
+parser.add_argument(
+    "--max_dataset_size",
+    type=int,
+    default=float("inf"),
+    help="Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.",
+)
+parser.add_argument(
+    "--preprocess",
+    type=str,
+    default="resize_and_crop",
+    help="scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]",
+)
+parser.add_argument(
+    "--no_flip",
+    action="store_true",
+    help="if specified, do not flip the images for data augmentation",
+)
+parser.add_argument(
+    "--norm",
+    type=str,
+    default="instance",
+    help="instance normalization or batch normalization",
+)
 
-parser.add_argument('--predict_depth', type=int, default=0, help='run geometry prediction on the generated images')
-parser.add_argument('--save_input', type=int, default=0, help='save input image')
-parser.add_argument('--reconstruct', type=int, default=0, help='get reconstruction')
-parser.add_argument('--how_many', type=int, default=100, help='number of images to test')
-parser.add_argument('--video', action='store_true', help='use video dataset')
+parser.add_argument(
+    "--predict_depth",
+    type=int,
+    default=0,
+    help="run geometry prediction on the generated images",
+)
+parser.add_argument("--save_input", type=int, default=0, help="save input image")
+parser.add_argument("--reconstruct", type=int, default=0, help="get reconstruction")
+parser.add_argument(
+    "--how_many", type=int, default=100, help="number of images to test"
+)
+parser.add_argument("--video", action="store_true", help="use video dataset")
 
 opt = parser.parse_args()
 print(opt)
@@ -66,7 +142,9 @@ if opt.cuda:
     if is_cuda_available:
         device = torch.device("cuda")
     else:
-        print("WARNING: You requested a CUDA device, but CUDA is not available -- using CPU instead")
+        print(
+            "WARNING: You requested a CUDA device, but CUDA is not available -- using CPU instead"
+        )
 elif is_cuda_available:
     print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
@@ -83,18 +161,24 @@ with torch.no_grad():
         net_GB.to(device)
         net_GB.load_state_dict(
             torch.load(
-                os.path.join(opt.checkpoints_dir, opt.name, 'netG_B_%s.pth' % opt.which_epoch),
+                os.path.join(
+                    opt.checkpoints_dir, opt.name, "netG_B_%s.pth" % opt.which_epoch
+                ),
                 map_location=device,
             )
         )
         net_GB.eval()
-    
+
     netGeom = 0
     if opt.predict_depth == 1:
         usename = opt.name
-        if (len(opt.geom_name) > 0) and (os.path.exists(os.path.join(opt.checkpoints_dir, opt.geom_name))):
+        if (len(opt.geom_name) > 0) and (
+            os.path.exists(os.path.join(opt.checkpoints_dir, opt.geom_name))
+        ):
             usename = opt.geom_name
-        myname = os.path.join(opt.checkpoints_dir, usename, 'netGeom_%s.pth' % opt.which_epoch)
+        myname = os.path.join(
+            opt.checkpoints_dir, usename, "netGeom_%s.pth" % opt.which_epoch
+        )
         netGeom = GlobalGenerator2(768, opt.geom_nc, n_downsampling=1, n_UPsampling=3)
 
         netGeom.load_state_dict(torch.load(myname, map_location=device))
@@ -103,31 +187,51 @@ with torch.no_grad():
 
         numclasses = opt.num_classes
         ### load pretrained inception
-        net_recog = InceptionV3(opt.num_classes, False, use_aux=True, pretrain=True, freeze=True, every_feat=opt.every_feat==1)
+        net_recog = InceptionV3(
+            opt.num_classes,
+            False,
+            use_aux=True,
+            pretrain=True,
+            freeze=True,
+            every_feat=opt.every_feat == 1,
+        )
         net_recog.to(device)
         net_recog.eval()
 
     # Load state dicts
     net_G.load_state_dict(
         torch.load(
-            os.path.join(opt.checkpoints_dir, opt.name, 'netG_A_%s.pth' % opt.which_epoch),
+            os.path.join(
+                opt.checkpoints_dir, opt.name, "netG_A_%s.pth" % opt.which_epoch
+            ),
             map_location=device,
         )
     )
-    print('loaded', os.path.join(opt.checkpoints_dir, opt.name, 'netG_A_%s.pth' % opt.which_epoch))
+    print(
+        "loaded",
+        os.path.join(opt.checkpoints_dir, opt.name, "netG_A_%s.pth" % opt.which_epoch),
+    )
 
     # Set model's test mode
     net_G.eval()
 
-    
-    transforms_r = [transforms.Resize(int(opt.size), Image.BICUBIC),
-                   transforms.ToTensor()]
+    transforms_r = [
+        transforms.Resize(int(opt.size), Image.BICUBIC),
+        transforms.ToTensor(),
+    ]
 
     dataset_cls = UnpairedDepthDataset
     if opt.video:
         dataset_cls = VideoDataset
-    test_data = dataset_cls(opt.dataroot, '', opt, transforms_r=transforms_r,
-                mode=opt.mode, midas=opt.midas>0, depthroot=opt.depthroot)
+    test_data = dataset_cls(
+        opt.dataroot,
+        "",
+        opt,
+        transforms_r=transforms_r,
+        mode=opt.mode,
+        midas=opt.midas > 0,
+        depthroot=opt.depthroot,
+    )
 
     dataloader = DataLoader(test_data, batch_size=opt.batchSize, shuffle=False)
 
@@ -142,37 +246,37 @@ with torch.no_grad():
 
     for i, batch in enumerate(dataloader):
         if i > opt.how_many:
-            break;
-        img_r  = Variable(batch['r']).to(device)
-        img_depth  = Variable(batch['depth']).to(device)
+            break
+        img_r = Variable(batch["r"]).to(device)
+        img_depth = Variable(batch["depth"]).to(device)
         real_A = img_r
 
-        name = batch['name'][0]
-        
+        name = batch["name"][0]
+
         input_image = real_A
         image = net_G(input_image)
-        save_image(image.data, full_output_dir+'/%s_out.png' % name)
+        save_image(image.data, full_output_dir + "/%s_out.png" % name)
 
-        if (opt.predict_depth == 1):
+        if opt.predict_depth == 1:
 
             geom_input = image
             if geom_input.size()[1] == 1:
                 geom_input = geom_input.repeat(1, 3, 1, 1)
             _, geom_input = net_recog(geom_input)
             geom = netGeom(geom_input)
-            geom = (geom+1)/2.0 ###[-1, 1] ---> [0, 1]
+            geom = (geom + 1) / 2.0  ###[-1, 1] ---> [0, 1]
 
             input_img_fake = channel2width(geom)
-            save_image(input_img_fake.data, full_output_dir+'/%s_geom.png' % name)
+            save_image(input_img_fake.data, full_output_dir + "/%s_geom.png" % name)
 
         if opt.reconstruct == 1:
             rec = net_GB(image)
-            save_image(rec.data, full_output_dir+'/%s_rec.png' % name)
+            save_image(rec.data, full_output_dir + "/%s_rec.png" % name)
 
         if opt.save_input == 1:
-            save_image(img_r, full_output_dir+'/%s_input.png' % name)
+            save_image(img_r, full_output_dir + "/%s_input.png" % name)
 
-        sys.stdout.write('\rGenerated images %04d of %04d' % (i, opt.how_many))
+        sys.stdout.write("\rGenerated images %04d of %04d" % (i, opt.how_many))
 
-    sys.stdout.write('\n')
+    sys.stdout.write("\n")
     ###################################
